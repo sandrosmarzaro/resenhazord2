f0a0a3182a4b285d94da8aab639d706628041b13
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-05 01:07:27 -0300

    fix: wrong using of array function and parameters of events

--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ async function connectToWhatsApp () {
     const { state, saveCreds } = await CreateAuthState.getAuthState()
     const sock = await CreateSock.getSock(state);
 
-    sock.ev.on('connection.update', ConnectionUpdate.run)
-    sock.ev.on('messages.upsert', MessageUpsert.run)
+    sock.ev.on('connection.update', update => ConnectionUpdate.run(update))
+    sock.ev.on('messages.upsert', message => MessageUpsert.run(message))
     sock.ev.on('connection.update', saveCreds)
 }
 
--- src/events/MessageUpsert.ts
+++ src/events/MessageUpsert.ts
@@ -1,7 +1,5 @@
 export class MessageUpsert {
-    public static async run() {
-        return async (message: any) => {
-            console.log(JSON.stringify(message, undefined, 2))
-        }
+    public static async run(message: any) {
+        console.log(JSON.stringify(message, undefined, 2))
     }
 }
