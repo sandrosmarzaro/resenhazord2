0896153bb2a7a2f04cb5dda01e1f775618671bb8
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-21 22:09:03 -0300

    build: disable full history and fix the wa version

--- src/infra/CreateSock.js
+++ src/infra/CreateSock.js
@@ -4,7 +4,9 @@ export default class CreateSock {
     constructor() {}
 
     static config = {
-        printQRInTerminal: true
+        printQRInTerminal: true,
+        syncFullHistory: false,
+        version: [2, 2413, 1]
     };
 
     static async getSock(state) {
