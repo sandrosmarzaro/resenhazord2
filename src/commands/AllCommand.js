974457247b70e08c04cab4bdc9c6840b1bf9ed8e
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-25 16:07:34 -0300

    feat: create all command

---
+++ src/commands/AllCommand.js
@@ -0,0 +1,27 @@
+export default class AllCommand {
+    static async run(data) {
+        console.log('ALL COMMAND');
+
+        const chat = await data.getChat(data);
+        if (!chat.isGroup) {
+            chat.sendMessage(
+                `Burro burro! Você só pode adicionar alguém em um grupo! 🤦‍♂️`,
+                { sendSeen: true, quotedMessageId: data.id._serialized }
+            );
+            return;
+        }
+
+        const participants = await chat.participants;
+        const text_inserted = data.body.replace(/\n*\s*\,\s*all\s*/, '');
+        let message = text_inserted.length > 0 ? text_inserted : '';
+        message += '\n\n';
+        for (const participant of participants) {
+            message += `@${participant.id.user} `;
+        }
+        const participants_ids = participants.map(participant => participant.id._serialized);
+        chat.sendMessage(
+            message,
+            { sendSeen: true, mentions: participants_ids}
+        );
+    }
+}
--- src/handlers/CommandHandler.js
+++ src/handlers/CommandHandler.js
@@ -1,4 +1,5 @@
 import AddCommand from '../commands/AddCommand.js';
+import AllCommand from '../commands/AllCommand.js';
 import BanCommand from '../commands/BanCommand.js';
 import MateusCommand from '../commands/MateusCommand.js';
 import OiCommand from '../commands/OiCommand.js';
@@ -14,6 +15,7 @@ export default class CommandHandler {
         const message = data.body;
         const handler = {
             ["^\\s*\\,\\s*add\\s*(?:\\d+)?\\s*$"]: AddCommand,
+            ["^\\s*\\,\\s*all\\s*"]: AllCommand,
             ["^\\s*\\,\\s*ban\\s*(?:\\@\\d+\\s*)*\\s*$"]: BanCommand,
             ["^\\s*\\,\\s*mateus\\s*$"]: MateusCommand,
             ["^\\s*\\,\\s*oi\\s*$"]: OiCommand,
