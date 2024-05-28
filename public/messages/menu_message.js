e439336514fdb3ca8d4c69ae0121be71820bf4d2
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-28 00:21:10 -0300

    feat: add message do menu

---
+++ public/messages/menu_message.js
@@ -0,0 +1,48 @@
+export default `\t\t\t📝 *MENU DE COMANDOS* 📝
+
+🫂 FUNÇÕES DE GRUPO 🫂
+
+- *,add* ou *,add* _28_ ou *,add* _28999999999_
+> Adiciona um número ao grupo. Aleatório ou específico.
+
+- *,adm*
+> Xingue aleatoriamente todos os administradores do grupo.
+
+- *,all* ou *,all* _mensagem_
+> Marca todos os participantes do grupo com ou sem uma mensagem.
+
+- *,ban* ou *,ban* _@pessoa_ _@pessoa_ ...
+> Remove aleatoriamente um ou especificamente um ou mais participantes do grupo.
+
+- *,fuck* _@pessoa_
+> Foda a pessoa mencioada mandando uma foto de pornozão pra ela.
+
+🎲 FUNÇÕES ALEATÓRIAS 🎲
+
+- *,d20*
+> Role um dado de vinte dimensões.
+
+- *,fato* ou *,fato* _hoje_
+> Descubra um fato aleatório ou de hoje em inglês.
+
+- *,mateus*
+> Descubra a probabilidade do Mateus nascer.
+
+- *,pokemon*
+> Receba uma imagem e dados de um pokémon aleatório.
+
+- *,porno* ou *,porno* _ia_
+> Receba um porno aleátório real ou feito por IA.
+
+- *,rule34*
+> Receba uma imagem aleatória da Rule 34.
+
+💾 FUNÇÕES DE DOWNLOAD 💾
+
+- *,tts* _mensagem_ ou *,tts* _en-us_ _mensagem_
+> Converta texto em audio usando a voz do Google, podendo trocar a língua.
+
+🙂 FUNÇÕES NORMAIS 🙂
+- *,oi*
+> Apenas diga oi ao bot.
+`
