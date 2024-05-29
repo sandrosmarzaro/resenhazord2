9fc7a38983851c421a8eb4241448c8dfe4fb4b15
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-29 00:09:55 -0300

    feat: use chorme in puppeter to add suport a video and gifs

--- src/infra/CreateClient.js
+++ src/infra/CreateClient.js
@@ -3,20 +3,26 @@ const { Client, LocalAuth } = pkg;
 
 export default class CreateClient {
 
-    static auth_path = './src/auth/session';
-
     static async getClient() {
+        const auth_path ='./src/auth/session';
+
+        const EXECUTABLE_PATHS = {
+            linux: '/usr/bin/google-chrome-stable',
+            darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+            win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+        };
+        const executablePath = EXECUTABLE_PATHS[process.platform];
+
         return new Client({
             puppeteer: {
-                headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
+                executablePath: executablePath
             },
             webVersionCache: {
                 remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html',
                 type: 'remote'
             },
             authStrategy: new LocalAuth({
-                dataPath: this.auth_path
+                dataPath: auth_path
             })
         });
     }
