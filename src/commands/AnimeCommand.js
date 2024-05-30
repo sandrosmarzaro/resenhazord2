1e2ccc4d78e41e6260a29949fad406f46181cba5
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-05-30 01:46:14 -0300

    feat: change api of anime command to add parameters and send real animes

--- src/commands/AnimeCommand.js
+++ src/commands/AnimeCommand.js
@@ -1,31 +1,60 @@
-import { NekosAPI } from "nekosapi";
 import pkg_wa from 'whatsapp-web.js';
 const { MessageMedia } = pkg_wa;
+import request_pkg from 'request';
+const request = request_pkg;
 
 export default class AnimeCommand {
 
-    static identifier = "^\\s*\\,\\s*anime\\s*$";
+    static identifier = "^\\s*\\,\\s*anime\\s*(?:nsfw|sfw)?\\s*$";
 
     static async run(data) {
         console.log('ANIME COMMAND');
 
         const chat = await data.getChat();
-        const nekos = new NekosAPI();
 
+        const sfw_tags = [
+            'waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick',
+            'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite',
+            'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke', 'dance', 'cringe'
+        ];
+        const nsfw_tags = ['waifu', 'neko', 'trap', 'blowjob'];
+
+        const rest_command = data.body.replace(/^\s*\,\s*anime\s*/, '');
+        const is_nsfw = rest_command.match(/nsfw/);
+
+        let tag;
+        is_nsfw ? tag = nsfw_tags[Math.floor(Math.random() * nsfw_tags.length)] : tag = sfw_tags[Math.floor(Math.random() * sfw_tags.length)];
+        let type;
+        is_nsfw ? type = 'nsfw' : type = 'sfw';
+
+        const url = `https://api.waifu.pics/${type}/${tag}`;
         try {
-            const image = await nekos.getRandomImage();
-            console.log('anime', image.image_url);
-            chat.sendMessage(
-                await MessageMedia.fromUrl(image.image_url),
-                {
-                    sendSeen: true,
-                    isViewOnce: true,
-                    quotedMessageId: data.id._serialized,
-                    caption: `Aqui está uma foto no estilo de anime para você! 😊`
+            request(url, async (error, response, body) => {
+                const anime = JSON.parse(body);
+                console.log('anime', anime);
+
+                if (error) {
+                    console.error('POKEMON COMMAND ERROR', error);
+                    return;
                 }
-            );
+
+                chat.sendMessage(
+                    await MessageMedia.fromUrl(anime.url),
+                    {
+                        sendSeen: true,
+                        isViewOnce: true,
+                        quotedMessageId: data.id._serialized,
+                        caption: `Aqui está uma foto de anime para você! 😊`
+                    }
+                );
+            });
         } catch (error) {
             console.error('ERROR ANIME COMMAND', error);
+
+            chat.sendMessage(
+                'Viiixxiii... Não consegui baixar a foto! 🥺👉👈',
+                { sendSeen: true, quotedMessageId: data.id._serialized }
+            );
         }
     }
 }
