f6cb5be3d673565dcf8d61fe2c137a273347988e
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-06-02 18:45:35 -0300

    chore: change log to only body

--- src/events/MessageEvent.js
+++ src/events/MessageEvent.js
@@ -5,7 +5,7 @@ export default class MessageEvent {
     static async run(message) {
         console.log('MESSAGE EVENT');
         if (!message.fromMe) {
-            console.log(JSON.stringify(message, null, 2));
+            console.log(JSON.stringify(message.body, null, 2));
         }
 
         const {RESENHA_ID, RESENHA_TEST_ID} = process.env;
