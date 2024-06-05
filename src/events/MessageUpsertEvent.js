f4f89bfcb11eae415beb00f65868d878215fa3d1
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-06-05 01:03:08 -0300

    fix: adjust strucutre to baileys

--- src/events/MessageUpsertEvent.js
+++ src/events/MessageUpsertEvent.js
@@ -5,14 +5,14 @@ export default class MessageUpsertEvent {
     static async run(data) {
         console.log('MESSAGE EVENT');
         console.log(JSON.stringify(data, null, 2));
-        return;
 
-        if (!message.fromMe) {
-            console.log(JSON.stringify(message.body, null, 2));
-        }
+        const [message] = data.messages;
+        // if (!message.fromMe) {
+        //     console.log(JSON.stringify(message.body, null, 2));
+        // }
 
-        const {RESENHA_ID, RESENHA_TEST_ID} = process.env;
-        const chat = message.id.remote;
+        const { RESENHA_ID, RESENHA_TEST_ID } = process.env;
+        const chat = message.key.remoteJid;
         if (!chat.includes(RESENHA_ID) && !chat.includes(RESENHA_TEST_ID)) {
             return;
         }
