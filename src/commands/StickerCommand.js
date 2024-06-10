5441c35e0cdb039b7f58a01939990580d2c04208
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-06-10 01:24:16 -0300

    fix: create sticker use third libary

--- src/commands/StickerCommand.js
+++ src/commands/StickerCommand.js
@@ -1,10 +1,12 @@
 import Resenhazord2 from "../models/Resenhazord2.js";
 import { downloadMediaMessage } from "@whiskeysockets/baileys";
-import sharp from "sharp";
+import { Sticker } from 'wa-sticker-formatter'
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+import ffmpeg from 'fluent-ffmpeg';
 
 export default class StickerCommand {
 
-    static identifier = "^\\s*\\,\\s*stic\\s*$";
+    static identifier = "^\\s*\\,\\s*stic\\s*(?:crop|full|circle|rounded)?\\s*$";
 
     static async run(data) {
         console.log('STICKER COMMAND');
@@ -13,7 +15,8 @@ export default class StickerCommand {
                     data.message?.extendedTextMessage?.contextInfo?.expiration;
 
         const has_upload_media = data?.message?.imageMessage || data?.message?.videoMessage;
-        const has_quoted_media = data?.message?.extendedTextMessage?.contextInfo?.quotedMessage?.imageMessage || data?.message?.extendedTextMessage?.contextInfo?.quotedMessage?.videoMessage;
+        const has_quoted_media = data?.message?.extendedTextMessage?.contextInfo?.quotedMessage?.imageMessage ||
+                                data?.message?.extendedTextMessage?.contextInfo?.quotedMessage?.videoMessage;
         if (!has_upload_media && !has_quoted_media) {
             Resenhazord2.socket.sendMessage(
                 data.key.remoteJid,
@@ -23,25 +26,35 @@ export default class StickerCommand {
             return;
         }
 
+        let rest_command = '';
+        if (data.message?.imageMessage?.caption) {
+            rest_command = data.message.imageMessage.caption.replace(/^\s*\,\s*stic\s*/, '');
+        }
+        if (data.message?.videoMessage?.caption) {
+            rest_command = data.message.videoMessage.caption.replace(/^\s*\,\s*stic\s*/, '');
+        }
+        const type = rest_command.length > 0 ? rest_command : 'full';
         try {
-            const buffer = await downloadMediaMessage(data);
-            console.log('BUFFER', buffer);
+            ffmpeg.setFfmpegPath(ffmpegPath);
+
+            const buffer = await downloadMediaMessage(data, 'buffer', {});
             if (buffer) {
-                const sticker_buffer = await sharp(buffer)
-                    .resize(512, 512, {
-                        fit: sharp.fit.cover,
-                        position: sharp.strategy.entropy
-                    })
-                    .webp({ quality: 50 })
+                const sticker = await new Sticker(buffer)
+                    .setPack('Resenhazord2')
+                    .setAuthor('Resenha')
+                    .setType(type)
+                    .setCategories(['Resenha', 'Bot'])
+                    .setQuality(100)
                     .toBuffer();
 
                 Resenhazord2.socket.sendMessage(
                     data.key.remoteJid,
-                    {sticker: sticker_buffer},
+                    {sticker: sticker},
                     {quoted: data, ephemeralExpiration: exp}
                 );
             }
-        } catch (error) {
+        }
+        catch (error) {
             console.error('ERROR STICKER COMMAND', error);
             Resenhazord2.socket.sendMessage(
                 data.key.remoteJid,
