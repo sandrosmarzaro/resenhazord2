44f8d00d06f1a1b7f5583a3ae30b7a44bae2e883
Sandro Smarzaro <sansmarzaro@gmail.com>
2024-06-10 23:46:30 -0300

    fix: decrease quality to gif conversion and add support to quoted

--- src/commands/StickerCommand.js
+++ src/commands/StickerCommand.js
@@ -1,5 +1,5 @@
 import Resenhazord2 from "../models/Resenhazord2.js";
-import { downloadMediaMessage } from "@whiskeysockets/baileys";
+import { downloadMediaMessage, generateWAMessageFromContent } from "@whiskeysockets/baileys";
 import { Sticker } from 'wa-sticker-formatter'
 import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
 import ffmpeg from 'fluent-ffmpeg';
@@ -33,26 +33,39 @@ export default class StickerCommand {
         if (data.message?.videoMessage?.caption) {
             rest_command = data.message.videoMessage.caption.replace(/^\s*\,\s*stic\s*/, '');
         }
+        if (has_quoted_media) {
+            rest_command = data.message.extendedTextMessage.text;
+        }
         const type = rest_command.length > 0 ? rest_command : 'full';
+
+        let message;
+        if (has_quoted_media) {
+            const quoted_message = data.message.extendedTextMessage.contextInfo.quotedMessage;
+            message = generateWAMessageFromContent(data.key.remoteJid, quoted_message, {
+                userJid: data.key?.remoteJid?.includes('@g.us') ? data.key.participant : data.key.remoteJid
+            });
+        }
+        else {
+            message = data;
+        }
         try {
             ffmpeg.setFfmpegPath(ffmpegPath);
 
-            const buffer = await downloadMediaMessage(data, 'buffer', {});
-            if (buffer) {
-                const sticker = await new Sticker(buffer)
-                    .setPack('Resenhazord2')
-                    .setAuthor('Resenha')
-                    .setType(type)
-                    .setCategories(['Resenha', 'Bot'])
-                    .setQuality(100)
-                    .toBuffer();
-
-                Resenhazord2.socket.sendMessage(
-                    data.key.remoteJid,
-                    {sticker: sticker},
-                    {quoted: data, ephemeralExpiration: exp}
-                );
-            }
+            const buffer = await downloadMediaMessage(message, 'buffer', {},  {
+                reuploadRequest: Resenhazord2.socket.updateMediaMessage
+            });
+            const sticker = await new Sticker(buffer)
+                .setPack('Resenhazord2')
+                .setAuthor('Resenha')
+                .setType(type)
+                .setCategories(['Resenha', 'Bot'])
+                .setQuality(50)
+                .build();
+            Resenhazord2.socket.sendMessage(
+                data.key.remoteJid,
+                {sticker},
+                {quoted: data, ephemeralExpiration: exp}
+            );
         }
         catch (error) {
             console.error('ERROR STICKER COMMAND', error);
