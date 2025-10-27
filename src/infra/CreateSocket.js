c92f4797720d5c3a07b19a2eefc2a1f1ecc7d669
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-10-27 15:37:03 -0300

    fix: use default browser config and add more settings

--- src/infra/CreateSocket.js
+++ src/infra/CreateSocket.js
@@ -5,24 +5,31 @@ export default class CreateSocket {
 
     static config = {
         qrTimeout: 300000,
-        browser: Browsers.baileys('Resenhazord2'),
-        printQRInTerminal: false,
+        browser: Browsers.macOS('Chrome'),
+        printQRInTerminal: true,
         syncFullHistory: false,
         logger: pino({
             transport: {
                 target: 'pino-pretty',
                 options: {
                     colorize: true,
-                    colorizeObjects: true
+                    colorizeObjects: true,
+                    translateTime: true
                 }
             },
-            level: 'error'
+            level: 'warn'
         }),
         generateHighQualityLinkPreview: true,
         retryRequestDelayMs: 2000,
         connectTimeoutMs: 60000,
         defaultQueryTimeoutMs: 60000,
-        emitOwnEvents: true
+        emitOwnEvents: true,
+        markOnlineOnConnect: false,
+        version: [2, 2323, 4],
+        browser: ['Chrome', 'Desktop', '1.0.0'],
+        linkPreviewImageThumbnailWidth: 192,
+        transactionOpts: { maxCommits: 10, delayMs: 3000 },
+        getMessage: async () => undefined
     };
 
     static async getSocket(state) {
