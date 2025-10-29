13d45f4ce38535f790c0a39a181f1286ec900342
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-10-29 19:43:19 -0300

    refactor: improve create socket without browser and changes pino

--- src/infra/CreateSocket.js
+++ src/infra/CreateSocket.js
@@ -1,38 +1,20 @@
-import { Browsers, makeWASocket } from "@whiskeysockets/baileys";
+import { makeWASocket } from "@whiskeysockets/baileys";
 import pino from "pino";
 
 export default class CreateSocket {
-
-    static config = {
-        qrTimeout: 300000,
-        browser: Browsers.macOS('Chrome'),
-        printQRInTerminal: true,
-        syncFullHistory: false,
-        logger: pino({
-            transport: {
-                target: 'pino-pretty',
-                options: {
-                    colorize: true,
-                    colorizeObjects: true,
-                    translateTime: true
-                }
-            },
-            level: 'warn'
-        }),
-        generateHighQualityLinkPreview: true,
-        retryRequestDelayMs: 2000,
-        connectTimeoutMs: 60000,
-        defaultQueryTimeoutMs: 60000,
-        emitOwnEvents: true,
-        markOnlineOnConnect: false,
-        version: [2, 2323, 4],
-        browser: ['Chrome', 'Desktop', '1.0.0'],
-        linkPreviewImageThumbnailWidth: 192,
-        transactionOpts: { maxCommits: 10, delayMs: 3000 },
-        getMessage: async () => undefined
-    };
-
     static async getSocket(state) {
-        return makeWASocket({ auth: state, ...this.config });
+
+        const config = {
+            auth: state,
+            logger: pino({
+                level: 'silent'
+            }),
+            qrTimeout: 60000,
+            syncFullHistory: false,
+            markOnlineOnConnect: false,
+            generateHighQualityLinkPreview: true,
+        };
+
+        return makeWASocket(config);
     }
 }
