8eb5a77cf14f9cd8801e9b07e08f6418758081f5
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-07 18:08:43 -0300

    feat(temp): artur commands only return adm command

--- src/handlers/CommandHandler.js
+++ src/handlers/CommandHandler.js
@@ -4,37 +4,44 @@ import path from 'path';
 import GetTextMessage from '../utils/GetTextMessage.js';
 import ReactMessage from '../utils/ReactMessage.js';
 import GetGroupExpiration from '../utils/GetGroupExpiration.js';
+import AdmCommand from '../commands/AdmCommand.js';
 
 export default class CommandHandler {
 
-    static async run(data) {
-        const text = GetTextMessage.run(data);
-        const handler = await this.import_comands();
+  static async run(data) {
+    const text = GetTextMessage.run(data);
+    const handler = await this.import_comands();
 
-        for (const [identifier, command] of Object.entries(handler)) {
-            if (new RegExp(identifier, 'i').test(text)) {
-                await ReactMessage.run(data);
-                command.run({
-                    ...data,
-                    text: text,
-                    expiration: await GetGroupExpiration.run(data)
-                });
-            }
+    for (const [identifier, command] of Object.entries(handler)) {
+      if (new RegExp(identifier, 'i').test(text)) {
+        await ReactMessage.run(data);
+        {
+          if (data?.key?.participantAlt == "86140065464518@lid") {
+            await AdmCommand.run({ ...data, text: text, expiration: await GetGroupExpiration.run(data) });
+            return;
+          }
+          await command.run({
+            ...data,
+            text: text,
+            expiration: await GetGroupExpiration.run(data)
+          });
         }
+      }
     }
+  }
 
     static async import_comands() {
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const commands_dir = path.resolve(__dirname, '../commands');
-        const files = await fs.readdir(commands_dir);
-        const handler = {};
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const commands_dir = path.resolve(__dirname, '../commands');
+    const files = await fs.readdir(commands_dir);
+    const handler = {};
 
-        for (const file of files) {
-            const { default: Command } = await import(`../commands/${file}`);
-            handler[Command.identifier] = Command;
-        }
-
-        return handler;
+    for (const file of files) {
+      const { default: Command } = await import(`../commands/${file}`);
+      handler[Command.identifier] = Command;
     }
+
+    return handler;
+  }
 }
