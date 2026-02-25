2c463109c5faa4599ff212c3b85628ce3b158a9b
Sandro Smarzaro <sansmarzaro@gmail.com>
2026-02-25 18:47:24 -0300

    feat: fetch updated WA version to create socket

--- src/infra/CreateSocket.js
+++ src/infra/CreateSocket.js
@@ -1,11 +1,13 @@
-import { makeWASocket } from '@whiskeysockets/baileys';
+import { makeWASocket, fetchLatestWaWebVersion } from '@whiskeysockets/baileys';
 import pino from 'pino';
 import groupMetadataCache from '../utils/GroupMetadataCache.js';
 
 export default class CreateSocket {
   static async getSocket(state) {
+    const { version } = await fetchLatestWaWebVersion();
     const config = {
       auth: state,
+      version,
       logger: pino({
         level: 'silent',
       }),
